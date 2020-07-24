477b7e63e1f9a362c36d11111707a3f924be2568
xulong <xulongeda@163.com>
2020-07-24 14:34:36 +0800

    add function fabs

--- appMain.c
+++ appMain.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 int main(int argc, char *argv[])
 {
 	printf("hello world\n");
+	float a = 12.34;
+	float b = 23.456;
+	if (fabs(a-b) < 1e-6)
+		printf("a == b\n");
+	else
+		printf("a != b\n");
 	return 0;
 }
